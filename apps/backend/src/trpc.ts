c5dfa320fe386bca9f7fec6974fc24a723fe5e02
gio-shara-code <giorgi.sharashenidze@code.berlin>
2024-01-17 14:11:06 +0100

    feat: added mutation and removed unused dep

--- apps/backend/src/trpc.ts
+++ apps/backend/src/trpc.ts
@@ -1,11 +1,10 @@
 // import superjson from 'superjson'
 import { initTRPC } from '@trpc/server'
-import { OpenApiMeta } from 'trpc-openapi'
 import { z, ZodError } from 'zod'
 
 // export const transformer = superjson
 
-export const t = initTRPC.meta<OpenApiMeta>().create({
+export const t = initTRPC.meta().create({
     // transformer,
     errorFormatter(opts) {
         const { shape, error } = opts
@@ -24,16 +23,18 @@ export const t = initTRPC.meta<OpenApiMeta>().create({
 })
 export const appRouter = t.router({
     getUser: t.procedure
-        // .meta({ /* 👉 */ openapi: { method: 'GET', path: '/say-hello' } })
         .input(z.string())
+        .output(z.object({ id: z.string(), name: z.string() }))
         .query((opts) => {
             opts.input
-            return { id: opts.input, name: 'Bilbo' }
+            return { id: opts.input, name: 'Love Trpc ❤️' }
         }),
     createUser: t.procedure
+        .meta({ openapi: { method: 'POST', path: '/createUser' } })
         .input(z.object({ name: z.string().min(5) }))
         .mutation(async (opts) => {
             // use your ORM of choice
+            console.log('opts', opts.input.name)
             return 'created!'
         }),
 })
--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -4,10 +4,24 @@ import { api } from '@/api/api'
 
 export default function Home() {
     const { data, isLoading } = api.getUser.useQuery('123234')
-    console.log('data:', data)
+    const {
+        mutateAsync,
+        isLoading: creatingUser,
+        isSuccess,
+    } = api.createUser.useMutation()
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             {isLoading ? 'Loading...' : data?.name}
+
+            {creatingUser && 'Creating user...'}
+
+            <button
+                onClick={async () => {
+                    await mutateAsync({ name: 'FooBar' })
+                }}
+            >
+                {isSuccess ? 'User Created!' : 'Create User'}
+            </button>
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     Get started by editing&nbsp;
