64c025b7a74134194e96328a157f8f9ddc5a184c
gio-shara-code <giorgi.sharashenidze@code.berlin>
2024-01-19 21:55:03 +0100

    chore: register form

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,27 +1,29 @@
 'use client'
-import Image from 'next/image'
 import { api } from '@/api/api'
+import { Button } from '@/components/ui/button'
+import { RegisterForm } from '@/components/common/RegisterForm'
 
 export default function Home() {
-    const { data, isLoading } = api.getUser.useQuery('123234')
-    const {
-        mutateAsync,
-        isLoading: creatingUser,
-        isSuccess,
-    } = api.createUser.useMutation()
+    // const { data, isLoading } = api.getUser.useQuery('123234')
+    // const {
+    //     mutateAsync,
+    //     isLoading: creatingUser,
+    //     isSuccess,
+    // } = api.createUser.useMutation()
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            {isLoading ? 'Loading...' : data?.name}
+        <main className={'page-base col-center'}>
+            {/*{isLoading ? 'Loading...' : data?.name}*/}
 
-            {creatingUser && 'Creating user...'}
+            {/*{creatingUser && 'Creating user...'}*/}
 
-            <button
-                onClick={async () => {
-                    await mutateAsync({ name: 'FooBar' })
-                }}
-            >
-                {isSuccess ? 'User Created!' : 'Create User'}
-            </button>
+            {/*<Button*/}
+            {/*    onClick={async () => {*/}
+            {/*        await mutateAsync({ name: 'FooBar' })*/}
+            {/*    }}*/}
+            {/*>*/}
+            {/*    {isSuccess ? 'User Created!' : 'Create User'}*/}
+            {/*</Button>*/}
+            <RegisterForm />
         </main>
     )
 }
