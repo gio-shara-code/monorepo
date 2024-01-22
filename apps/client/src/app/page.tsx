bcdb16889ee6e182b47fc51e3799ba2d2d059041
gio-shara-code <giorgi.sharashenidze@code.berlin>
2024-01-22 14:55:53 +0400

    test: implemented user fetch

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -2,6 +2,7 @@
 import { api } from '@/api/api'
 import { Button } from '@/components/ui/button'
 import { RegisterForm } from '@/components/common/RegisterForm'
+import { useState } from 'react'
 
 export default function Home() {
     // const { data, isLoading } = api.getUser.useQuery('123234')
@@ -10,6 +11,11 @@ export default function Home() {
     //     isLoading: creatingUser,
     //     isSuccess,
     // } = api.createUser.useMutation()
+    const [fetchUser, setFetchUser] = useState(false)
+    const { data } = api.userRouter.getMyProfile.useQuery(undefined, {
+        enabled: fetchUser,
+    })
+    console.log('data', data)
     return (
         <main className={'page-base col-center'}>
             {/*{isLoading ? 'Loading...' : data?.name}*/}
@@ -24,6 +30,8 @@ export default function Home() {
             {/*    {isSuccess ? 'User Created!' : 'Create User'}*/}
             {/*</Button>*/}
             <RegisterForm />
+            <Button onClick={() => setFetchUser(true)}>Fetch User</Button>
+            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
         </main>
     )
 }
